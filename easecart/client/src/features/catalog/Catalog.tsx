fc14ecd33748880cacc8dfcb1ad240e04fdd5381
mcaks <miha.caks1@student.um.si>
2024-06-25 00:36:34 +0200

    modify catalog and product details

--- easecart/client/src/features/catalog/Catalog.tsx
+++ easecart/client/src/features/catalog/Catalog.tsx
@@ -1,17 +1,23 @@
 import { useState, useEffect } from "react";
 import { Product } from "../../app/models/product";
-import ProductList from "./ProductList.tsx";
+import ProductList from "./ProductList";
+import agent from "../../app/api/agent";
+import Spinner from "../../app/layout/Spinner";
 
 export default function Catalog(){
-const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
   useEffect(()=>{
-    fetch('http://localhost:8081/api/products')
-    .then(response =>response.json())
-    .then(data=>setProducts(data.content));
-  },[]);
+    agent.Store.list()
+        .then((products)=>setProducts(products.content))
+        .catch(error=>console.log(error))
+        .finally(()=>setLoading(false));
+  }, []);
+  if(!products) return <h3>Unable to load Products</h3>
+  if(loading) return <Spinner message='Loading Products...'/>
   return (
-    <>
-      <ProductList products={products}/>
-    </>
+      <>
+        <ProductList products={products}/>
+      </>
   )
 }
--- easecart/client/src/features/catalog/ProductDetails.tsx
+++ easecart/client/src/features/catalog/ProductDetails.tsx
@@ -3,6 +3,9 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
+import agent from "../../app/api/agent";
+import NotFound from "../../app/errors/NotFoundError";
+import Spinner from "../../app/layout/Spinner";
 
 export default function ProductDetails(){
     const {id} = useParams<{id:string}>();
@@ -10,60 +13,60 @@ export default function ProductDetails(){
     const [loading, setLoading] = useState(true);
     const extractImageName = (item: Product): string | null =>{
         if(item && item.pictureUrl){
-          const parts = item.pictureUrl.split('/');
-          if(parts.length>0){
-            return parts[parts.length-1];
-          }
+            const parts = item.pictureUrl.split('/');
+            if(parts.length>0){
+                return parts[parts.length-1];
+            }
         }
         return null;
-      }
-      const formatPrice = (price: number): string =>{
+    }
+    const formatPrice = (price: number): string =>{
         return new Intl.NumberFormat('en-In', {
-          style:'currency',
-          currency: 'EUR',
-          minimumFractionDigits: 2
+            style:'currency',
+            currency: 'EUR',
+            minimumFractionDigits: 2
         }).format(price);
-      }
+    }
     useEffect(()=>{
-        axios.get(`http://localhost:8081/api/products/${id}`)
-        .then(response=>setProduct(response.data))
-        .catch(error=>console.error(error))
-        .finally(()=>setLoading(false));
+        id && agent.Store.details(parseInt(id))
+            .then(response=>setProduct(response))
+            .catch(error=>console.error(error))
+            .finally(()=>setLoading(false));
     }, [id])
-    if(loading) return <h3>Loading Product...</h3>
-    if(!product) return <h3>Product not found</h3>
+    if(loading) return <Spinner message='Loading Product...'/>
+    if(!product) return <NotFound/>
     return(
         <Grid container spacing={6}>
-        <Grid item xs={6}>
-            <img src={"/images/products/"+extractImageName(product)} alt={product.name} style={{width: '100%'}}/>
+            <Grid item xs={6}>
+                <img src={"/images/products/"+extractImageName(product)} alt={product.name} style={{width: '100%'}}/>
+            </Grid>
+            <Grid item xs={6}>
+                <Typography variant='h3'>{product.name}</Typography>
+                <Divider sx={{mb:2}}/>
+                <Typography gutterBottom color='secondary' variant="h4">{formatPrice(product.price)}</Typography>
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{product.name}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Description</TableCell>
+                                <TableCell>{product.description}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Type</TableCell>
+                                <TableCell>{product.productType}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Brand</TableCell>
+                                <TableCell>{product.productBrand}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
         </Grid>
-        <Grid item xs={6}>
-            <Typography variant='h3'>{product.name}</Typography>
-            <Divider sx={{mb:2}}/>
-            <Typography gutterBottom color='secondary' variant="h4">{formatPrice(product.price)}</Typography>
-            <TableContainer>
-                <Table>
-                    <TableBody>
-                        <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell>{product.name}</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>Description</TableCell>
-                            <TableCell>{product.description}</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>Type</TableCell>
-                            <TableCell>{product.productType}</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>Brand</TableCell>
-                            <TableCell>{product.productBrand}</TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Grid>
-    </Grid>
     )
 }
