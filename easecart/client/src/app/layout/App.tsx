b5ff896adfe3a2d090be50dfa0870193c7d34cef
mcaks <miha.caks1@student.um.si>
2024-06-25 00:50:35 +0200

    toast nottifications

--- easecart/client/src/app/layout/App.tsx
+++ easecart/client/src/app/layout/App.tsx
@@ -1,7 +1,9 @@
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-import Catalog from "../../features/catalog/Catalog";
-import Header from "./Header.tsx";
+import Header from "./Header";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
     const [darkMode, setDarkMode] = useState(false);
@@ -17,10 +19,11 @@ function App() {
     }
     return (
         <ThemeProvider theme={theme}>
+            <ToastContainer position="bottom-right" hideProgressBar theme="colored"/>
             <CssBaseline/>
             <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
             <Container sx={{ paddingTop: "64px" }}>
-                <Catalog/>
+                <Outlet/>
             </Container>
         </ThemeProvider>
     )
