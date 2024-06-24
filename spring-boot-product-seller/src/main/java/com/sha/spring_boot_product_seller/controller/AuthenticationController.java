1512949230bb39ba1b2558c68f75201321a53086
mcaks <miha.caks1@student.um.si>
2024-06-24 15:34:00 +0200

    endpoints login, signup

---
+++ spring-boot-product-seller/src/main/java/com/sha/spring_boot_product_seller/controller/AuthenticationController.java
@@ -0,0 +1,15 @@
+package com.sha.spring_boot_product_seller.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/authentication")
+public class AuthenticationController{
+
+}
