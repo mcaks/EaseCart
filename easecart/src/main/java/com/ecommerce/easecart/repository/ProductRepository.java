b67c5f5b34c4e45d3adc9daa5d3d4fd6df3cfadd
mcaks <miha.caks1@student.um.si>
2024-06-24 21:19:22 +0200

    product repo + impl

--- easecart/src/main/java/com/ecommerce/easecart/repository/ProductRepository.java
+++ easecart/src/main/java/com/ecommerce/easecart/repository/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.ecommerce.easecart.repository;
 
 import com.ecommerce.easecart.entitiy.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Page<Product> findAll(Specification<Product> spec, Pageable pageable);
+    Specification<Product> searchByNameContaining(String keyword);
+    Specification<Product> findByBrandId(Integer brandId);
+    Specification<Product> findByTypeId(Integer typeId);
+    Specification<Product> findByBrandIdAndTypeId(Integer brandId, Integer typeId);
 }
